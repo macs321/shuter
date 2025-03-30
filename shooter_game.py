61c46b72cfce9a2427b15c38b636b8efd1257640
macs321 <macs321@gmail.com>
2025-03-30 13:29:26 +0300

    ad lives,lose and win,font lose and win,ad asteroid

--- shooter_game.py
+++ shooter_game.py
@@ -9,7 +9,7 @@ FPS = 60
 
 score = 0
 lost = 0
-
+lives = 100000
 
 
 win = pygame.display.set_mode(SIZE)
@@ -51,9 +51,9 @@ class Player(GameSprite):
         if keys[pygame.K_DOWN] and self.rect.y <= HEIGHT - self.rect.height:
             self.rect.y += 5
         if keys[pygame.K_RIGHT]and self.rect.x <= WIDTH - self.rect.width:
-            self.rect.x += 7
+            self.rect.x += 10
         if keys[pygame.K_LEFT]and self.rect.x >= 0:
-            self.rect.x -= 7
+            self.rect.x -= 10
     def fire(self):
         new_bullet = Bullet("bullet.png",
                              (15,20),
@@ -75,6 +75,9 @@ class Enemy(GameSprite):
             global lost
             lost += 1
 
+ # if self.rect.y = self.bullets and self.rect.x = self.bullets:
+
+
 class Bullet(GameSprite):
     def update(self):
         self.rect.y -= self.speed
@@ -85,7 +88,7 @@ class Bullet(GameSprite):
 pygame.font.init()
 
 medium_font = pygame.font.SysFont("Helvetica", 24)
-
+big_font = pygame.font.SysFont("Impact",24)
 
 player =Player("rocket.png",(50,70),(HEIGHT//2,WIDTH//2),10)
 
@@ -93,11 +96,19 @@ enemies = pygame.sprite.Group()
 enemies_num = 3
 
 for i in range(enemies_num):
-    new_enemy = Enemy("ufo.png",
-                       (70,50),
-                       (random.randint(50,WIDTH-50),0),
-                       random.randint(7,13)
-                       )
+    n = random.randint(1,1000)
+    if n >200:
+        new_enemy = Enemy("ufo.png",
+                    (70,50),
+                    (random.randint(50,WIDTH-50),0),
+                    random.randint(7,13)
+                    )
+    else:
+        new_enemy =Enemy("asteroid.png",
+                        (70,50),
+                        (random.randint(50,WIDTH-50),0),
+                        random.randint(7,13)
+                        )
     enemies.add(new_enemy)
 
 game = True
@@ -120,8 +131,13 @@ while game:
         lost_text = medium_font.render("пропущено зелених:" + str(lost), True, (255,255,255))
         win.blit(lost_text,(0,0))
 
-        score = medium_font.render("Збито зелених:" + str(0), True, (255,255,255))
-        win.blit(score,(0,25))
+        score_text = medium_font.render("Збито зелених:" + str(score), True, (255,255,255))
+        win.blit(score_text,(0,25))
+
+        lives_text = medium_font.render("життя" + str(lives), True, (255,25,55))
+        win.blit(lives_text,(WIDTH-100,0))
+
+
 
         player.update()
         player.reset(win)
@@ -130,6 +146,57 @@ while game:
         bullets.update()
         bullets.draw(win)
 
+        
+
+        collided = pygame.sprite.groupcollide(enemies,bullets,True,True)
+        for dead_enemy in collided:
+            score += 1
+            n = random.randint(1,1000)
+            if n >200:
+                new_enemy = Enemy("ufo.png",
+                                (70,50),
+                            (random.randint(50,WIDTH-50),0),
+                            random.randint(7,13)
+                            )
+            else:
+                new_enemy =Enemy("asteroid.png",
+                                (70,50),
+                                (random.randint(50,WIDTH-50),0),
+                                random.randint(7,13)
+                                )
+            enemies.add(new_enemy)
+            
+
+        if score >= 200:
+            finish = True
+            win_text = big_font.render("Ти переміг ти втік від Порошенка",True,(0,255,0))
+            win.blit(win_text,(WIDTH//2-200,HEIGHT//2))
+
+            
+        if  lost >= 200 or lives <= 0:
+            finish = True
+            lodt_text = big_font.render("Порошенко і його цукерки,не ростраюйся",True,(255,0,0))
+            win.blit(lodt_text,(WIDTH//2-250,HEIGHT//2))
+
+    collided = pygame.sprite.spritecollide(player,enemies, True)
+    for enemy in collided:
+        lives -= 1
+        n = random.randint(1,1000)
+        if n >200:
+            new_enemy = Enemy("ufo.png",
+                            (70,50),
+                        (random.randint(50,WIDTH-50),0),
+                        random.randint(7,13)
+                        )
+        else:
+            new_enemy =Enemy("asteroid.png",
+                            (70,50),
+                            (random.randint(50,WIDTH-50),0),
+                            random.randint(7,13)
+                            )
+        enemies.add(new_enemy)
+
+ 
 
     pygame.display.update()
 
